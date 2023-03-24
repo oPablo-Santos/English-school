7618e27abb67b8eab6e39af4eef9ef86f4ae897a
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-03-24 09:54:41 -0300

    Update

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 const app = express();
 
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get("/teste", (req, res) => {
-  res.status(200).send({ mensagem: "Boas vindas a API" });
-});
+routes(app)
 
 app.listen(port, () => console.log(`listening on port ${port}`));
+
+module.exports = app;
