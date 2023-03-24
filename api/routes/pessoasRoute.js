dc0fb68ff5a0d9fb1f03c73b17b5938869d6e753
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-03-24 09:55:21 -0300

    update

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
+
+const router = Router();
+
+router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoas);
+
+module.exports = router;
